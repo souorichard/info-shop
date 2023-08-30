36e6b1e4062364f5335ba218e9c6581e45baadb6
souorichard <elaine.ra96@gmail.com>
2023-08-30 15:47:37 -0300

    Alteração básica

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-
+import Icon from 'react-native-vector-icons/Feather';
+;
 export default function Header() {
   return (
     <View style={styles.header}>
+      <Icon name="shopping-bag" size={28} color="#F00" />
       <Text style={styles.title}>Info <Text style={styles.title2}>Shop</Text></Text>
     </View>
   );
@@ -12,12 +14,13 @@ export default function Header() {
 const styles = StyleSheet.create({
   header: {
     width: "100%",
-    height: 80,
+    height: 70,
     paddingVertical: 16,
     paddingHorizontal: 24,
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
+    gap: 14,
     backgroundColor: "#0A0F24"
   },
 
